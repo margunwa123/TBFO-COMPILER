1e9b413cae2926c76241d9462f7987dcd2b25737
Mario Gunawan <margunwa123@yahoo.com>
2019-11-25 21:09:01 +0700

    ngecopy parser dari pak Robert

--- compiler.py
+++
@@ -1,8 +0,0 @@
-def readfile(filename:str):
-    fo = open(filename,'r')
-    content = fo.readlines()
-    print(content)
-    fw = open(filename+'hasil','w')
-    fw.write(content)
-
-readfile('grammar.txt')

2b885060ebbb3f837dd0db84343cbe866d818d26
Mario Gunawan <margunwa123@yahoo.com>
2019-11-27 01:19:21 +0700

    PUJA ROBERT

--- cek.py
+++ cek.py
@@ -1,11 +1,3 @@
-def hello(a,b):
-    a = a+2
-    b = b+2
-    return a,b
-
-asd = hello(5,4)
-print(asd[1])
-d = 1
-kontol = []
-kontol.append(1)
-print(kontol[0])
+for i in range(5):
+    print(i)
+    
--- tes.py
+++
@@ -1,3 +0,0 @@
-a = [1,2,3,1,2]
-for i in (a):
-    print('haha')
